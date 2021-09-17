a9595d6686f94788aa04ecfe8316ca194c781868
Ashutosh Pandey <rojha2030@gmail.com>
2021-09-17 23:07:06 +0530

    initial commit otp funcionality added

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// react router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//components
+import SignIn from "./Components/PhoneOtp/SignIn";
+import Home from "./Components/PhoneOtp/Home";
+
+const App = () => {
+  const [viewOtpForm, setViewOtpForm] = useState(false);
+  const [user, setUser] = useState(false);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCUdDvqomv-YwURzNRMkObIaV9uxwoel4I",
+    authDomain: "trinkerr-project-65b94.firebaseapp.com",
+    projectId: "trinkerr-project-65b94",
+    storageBucket: "trinkerr-project-65b94.appspot.com",
+    messagingSenderId: "355167737303",
+    appId: "1:355167737303:web:4102977f3ac3db5a9d6792",
+    measurementId: "G-ZFH1KLFKL9",
+  };
+
+  useEffect(() => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: function (response) {
+          console.log("Captcha Resolved");
+          this.onSignInSubmit();
+        },
+        defaultCountry: "IN",
+      }
+    );
+  }, []);
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+
+  const auth = firebase.auth();
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUser(user);
+    }
+  });
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+
+    let phone_number = "+91" + e.target.phone.value;
+    const appVerifier = window.recaptchaVerifier;
+
+    auth
+      .signInWithPhoneNumber(phone_number, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        console.log("otp sent");
+        setViewOtpForm(true);
+        window.confirmationResult = confirmationResult;
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+        alert(error.message);
+      });
+  };
+
+  const otpSubmit = (e) => {
+    e.preventDefault();
+
+    let opt_number = e.target.otp_value.value;
+
+    window.confirmationResult
+      .confirm(opt_number)
+      .then((confirmationResult) => {
+        console.log(confirmationResult);
+        console.log("success");
+        window.open("/", "_self");
+      })
+      .catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        alert(error.message);
+      });
+  };
+
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        window.open("/signin", "_self");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+
+  return (
+    <Router>
+      <div id="recaptcha-container"></div>
+      <Switch>
+        <Route path="/" exact>
+          <Home signOut={signOut} user={user} />
+        </Route>
+        <Route path="/signin" exact>
+          <SignIn
+            loginSubmit={loginSubmit}
+            otpSubmit={otpSubmit}
+            viewOtpForm={viewOtpForm}
+          />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
