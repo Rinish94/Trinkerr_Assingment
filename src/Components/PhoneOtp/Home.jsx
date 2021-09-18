032ea312a91f4d40f3d229c90437f398f06c35e5
Ashutosh Pandey <rojha2030@gmail.com>
2021-09-18 12:38:05 +0530

    authentication proceess done working on homepage now

--- src/Components/PhoneOtp/Home.jsx
+++ src/Components/PhoneOtp/Home.jsx
@@ -1,13 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useHistory } from "react-router";
 
-const Home = ({ signOut, user }) => {
-  console.log(user);
+const imagesPool = [
+  "http://getdrawings.com/free-icon-bw/one-icon-4.png",
+  "http://getdrawings.com/free-icon-bw/free-shirt-icon-9.png",
+  "http://getdrawings.com/free-icon-bw/serial-number-icon-19.png",
+  "http://getdrawings.com/free-icon-bw/serial-number-icon-18.png",
+  "http://getdrawings.com/free-icon-bw/number-one-icon-17.png",
+];
+
+const Home = () => {
+  const [count, setCount] = useState(0);
+  const history = useHistory();
+  const firebaseConfig = {
+    apiKey: "AIzaSyCUdDvqomv-YwURzNRMkObIaV9uxwoel4I",
+    authDomain: "trinkerr-project-65b94.firebaseapp.com",
+    projectId: "trinkerr-project-65b94",
+    storageBucket: "trinkerr-project-65b94.appspot.com",
+    messagingSenderId: "355167737303",
+    appId: "1:355167737303:web:4102977f3ac3db5a9d6792",
+    measurementId: "G-ZFH1KLFKL9",
+  };
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+  const localData = JSON.parse(localStorage.getItem("auth")) || undefined;
+
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        window.open("/", "_self");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+    localStorage.setItem("auth", JSON.stringify({ status: false }));
+  };
+
+  const imageForward = () => {
+    if (count > count.length - 1) {
+      setCount(0);
+    } else {
+      setCount(count + 1);
+    }
+  };
+  // console.log(count);
+  //  imageForward
+  const imageBackward = () => {
+    if (count <= 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
+  };
+  (!localData?.status || localData === undefined) && history.push("/"); //
+
   return (
     <div className="wrapper">
-      <h1 className="main-heading">Welcome 👋, {user.phoneNumber}</h1>
+      <h1 className="main-heading">Welcome 👋, {localData?.name}</h1>
       <button className="main-button" id="signOut" onClick={signOut}>
         Sign Out
       </button>
+      <div>
+        <img width="50px" height="50px" src={imagesPool[count]} alt="images" />
+        <div>
+          <button onClick={imageBackward} disabled={count === 0}>
+            {"<"}
+          </button>
+          <button
+            onClick={imageForward}
+            disabled={count === imagesPool.length - 1}
+          >
+            {">"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/PhoneOtp/SignIn.jsx
+++ src/Components/PhoneOtp/SignIn.jsx
@@ -1,6 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import firebase from "firebase/app";
+import "firebase/auth";
+//components
+// import SignIn from "./Components/PhoneOtp/SignIn";
+// import Home from "./Components/PhoneOtp/Home";
 
-const SignIn = ({ loginSubmit, otpSubmit, viewOtpForm }) => {
+const SignIn = () => {
+  const [viewOtpForm, setViewOtpForm] = useState(false);
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(false);
+  let [login, setLogin] = useState(false);
+  const [names, setNames] = useState("");
+  const history = useHistory();
+  const localData = JSON.parse(localStorage.getItem("auth")) || undefined;
+  localData && history.push("/home");
+  const firebaseConfig = {
+    apiKey: "AIzaSyCUdDvqomv-YwURzNRMkObIaV9uxwoel4I",
+    authDomain: "trinkerr-project-65b94.firebaseapp.com",
+    projectId: "trinkerr-project-65b94",
+    storageBucket: "trinkerr-project-65b94.appspot.com",
+    messagingSenderId: "355167737303",
+    appId: "1:355167737303:web:4102977f3ac3db5a9d6792",
+    measurementId: "G-ZFH1KLFKL9",
+  };
+
+  useEffect(() => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: function (response) {
+          console.log("Captcha Resolved");
+          this.onSignInSubmit();
+        },
+        defaultCountry: "IN",
+      }
+    );
+  }, []);
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+
+  const auth = firebase.auth();
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUser(user);
+    }
+    console.log(user);
+  });
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    let nameData = e.target.name.value;
+    // console.log(nameData);
+    setNames(nameData);
+    let phone_number = "+91" + e.target.phone.value;
+    const appVerifier = window.recaptchaVerifier;
+
+    auth
+      .signInWithPhoneNumber(phone_number, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        console.log("otp sent");
+        setViewOtpForm(true);
+        setLoading(true);
+        window.confirmationResult = confirmationResult;
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+        alert(error.message);
+      });
+  };
+
+  const otpSubmit = (e) => {
+    e.preventDefault();
+
+    let opt_number = e.target.otp_value.value;
+
+    window.confirmationResult
+      .confirm(opt_number)
+      .then((confirmationResult) => {
+        setLoading(false);
+        console.log(confirmationResult);
+        console.log("success");
+        localStore();
+        history.push("/home", "_self");
+      })
+      .catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        alert(error.message);
+      });
+  };
+  const localStore = () => {
+    setLogin((login = true)); ///forced assingment
+    const payload = {
+      status: login,
+      name: names,
+    };
+    localStorage.setItem("auth", JSON.stringify(payload));
+  };
+
+  // console.log(login)
   return (
     <div className="wrapper">
       <h1 className="main-heading">Sign in</h1>
@@ -8,6 +116,15 @@ const SignIn = ({ loginSubmit, otpSubmit, viewOtpForm }) => {
       {!viewOtpForm ? (
         <div className="form-wrapper">
           <form id="loginForm" onSubmit={loginSubmit}>
+            <div className="input-field">
+              <label>User Name</label>
+              <input
+                type="text"
+                placeholder="name"
+                name="name"
+                autoComplete="false"
+              />
+            </div>
             <div className="input-field">
               <label>Phone Number</label>
               <input
@@ -21,6 +138,7 @@ const SignIn = ({ loginSubmit, otpSubmit, viewOtpForm }) => {
               Sign in
             </button>
           </form>
+          {loading && <h2>Please Wait</h2>}
         </div>
       ) : (
         <div className="form-wrapper" onSubmit={otpSubmit}>
